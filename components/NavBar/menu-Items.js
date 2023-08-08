f9b7bfcb50718e5b28c23dc5eb7035f9699903f5
KHUSHBU NAIK <khushbu.naik@scet.ac.in>
2023-08-08 12:36:19 +0530

    All menus are added

--- components/NavBar/menu-Items.js
+++ components/NavBar/menu-Items.js
@@ -46,35 +46,208 @@ export const menu_Items = [
         url: ''
       },
       {
+        title: 'Continous Evaluation Scheme',
+        url: ''
+      },
+      {
         title: 'TimeTable',
         url: ''
       },
       {
-        title: 'TimeTable1',
+        title: 'Syllabus and Practical List',
         url: ''
       },
-
+      {
+        title: 'Lab Manual',
+        url: ''
+      },
+      {
+        title: 'GTU Exam Papers',
+        url: ''
+      },
+      {
+        title: 'External Practical Exam Time Table',
+        url: ''
+      },
+      {
+        title: 'GTU Midterm Exam Conduction',
+        url: ''
+      },
+      {
+        title: 'Result Analysis',
+        url: ''
+      },
+      {
+        title: 'Projects',
+        url: ''
+      },
+      {
+        title: 'Design Engineering',
+        url: ''
+      },
+      {
+        title: 'Online Resources',
+        url: ''
+       },
     ]
   },
   {
     title: 'Industrial Interaction ',
     url: '/about',
+    submenu: [
+      {
+        title: 'T&P Data',
+        url: ''
+      },
+      {
+        title: 'Industrial Visit',
+        url: ''
+      },
+      {
+        title: 'Industrial Training',
+        url: ''
+      },
+      {
+        title: 'Internship (Final Semester)',
+        url: ''
+      },
+      {
+        title: 'Company MOUs',
+        url: ''
+      },
+    ]
   },
   {
     title: 'Student Corner ',
     url: '/about',
+    submenu: [
+      {
+        title: 'Student Information',
+        url: ''
+      },
+      {
+        title: 'Student Achievements',
+        url: ''
+      },
+      {
+        title: 'Scholarships',
+        url: ''
+      },
+      {
+        title: 'Alumni Data',
+        url: ''
+      },
+      {
+        title: 'IEP Students',
+        url: ''
+      },
+      {
+        title: 'Foreign Students',
+        url: ''
+      }
+    ]
   },
   {
     title: 'Events ',
     url: '/about',
+    submenu: [
+      {
+        title: 'Converse',
+        url: ''
+      },
+      {
+        title: 'Expert Talks/ Workshops/ STTPs',
+        url: ''
+      },
+      {
+        title: 'IT Olympics',
+        url: ''
+      },
+      {
+        title: 'E-Magazines',
+        url: ''
+      },
+      {
+        title: 'Alumni Talk',
+        url: ''
+      },
+      {
+        title: 'Media Coverage',
+        url: ''
+      },
+      {
+        title: 'CSI Chapter',
+        url: ''
+      },
+    ]
   },
   {
     title: 'Library ',
     url: '/about',
+    submenu: [
+      {
+        title: 'Central Library Access',
+        url: ''
+      },
+      {
+        title: 'Books in Dept',
+        url: ''
+      },
+      {
+        title: 'CDs in Dept',
+        url: ''
+      },
+      {
+        title: 'Central Library Info',
+        url: ''
+      },
+      {
+        title: 'Library Manual',
+        url: ''
+      },
+    ]
   },
   {
     title: 'Quick Links ',
     url: '/about',
+    submenu: [
+      {
+        title: 'List of Holidays-2023',
+        url: ''
+      },
+      {
+        title: 'Department Committies 2023-24',
+        url: ''
+      },
+      {
+        title: 'CR LR Candidate form',
+        url: ''
+      },
+      {
+        title: 'SCET Leave Management',
+        url: ''
+      },
+      {
+        title: 'Intra Phone Directory',
+        url: ''
+      },
+      {
+        title: 'College Intranet',
+        url: ''
+      },
+      {
+        title: 'College Library',
+        url: ''
+      },
+      {
+        title: 'Keri Firewall',
+        url: ''
+      },
+      {
+        title:'Policy for Internet Usage',
+        url:''
+      },
+    ]
   },
 ];
 
