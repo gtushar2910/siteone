bbd83241483b30e12c9113fa7fe8fa4e451e94b8
Tushar Gohil <tushar.gohil@scet.ac.in>
2023-08-14 15:07:54 +0530

    About Dept Completed 7

--- app/api/staff/getStaffList/route.js
+++ app/api/staff/getStaffList/route.js
@@ -1,8 +1,9 @@
-import prisma from "@/lib/prisma"
+import { PrismaClient } from '@prisma/client'
 import { NextResponse } from "next/server";
 
 export async function GET(request)
 {
+  let prisma = new PrismaClient();
   const staffs = await prisma.staff.findMany({
     orderBy: {
               seqnum: 'asc',
