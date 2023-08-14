254cbba2fa856add06fe3c30c6455e445da71575
Tushar Gohil <tushar.gohil@scet.ac.in>
2023-08-14 10:51:59 +0530

    About Dept Completed

--- app/layout.js
+++ app/layout.js
@@ -14,7 +14,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
 
-    <html lang="en">
+    <html lang="en" className={inter.className}>
       <body className="h-screen">
         <NextUIProvider>
           <Header></Header>
