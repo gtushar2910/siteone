490cb4770093eb4291ed9cf8908fb48c5a515213
Tushar Gohil <tushar.gohil@scet.ac.in>
2023-09-12 14:22:00 +0530

    error solved

--- app/Staff/changepwd/page.js
+++ app/Staff/changepwd/page.js
@@ -10,13 +10,6 @@ import {
 } from "@nextui-org/react";
 import axios from 'axios';
 
-
-export const getServerSideProps = async () => {
-    const res = await fetch('https://api.github.com/repos/vercel/next.js')
-    const repo = await res.json()
-    return { props: { repo } }
-  }
-
 const ChangePassword = () => {
 
     const [error, setError] = useState(false)
