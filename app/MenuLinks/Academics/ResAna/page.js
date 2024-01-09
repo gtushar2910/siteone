ed4824f291e762ecb078545ab6865e8d4f701413
Tushar Gohil <tushar.gohil@scet.ac.in>
2024-01-09 18:01:53 +0530

    Result Analysis

--- app/MenuLinks/Academics/ResAna/page.js
+++ app/MenuLinks/Academics/ResAna/page.js
@@ -1,137 +1,127 @@
 "use client"
-import React from 'react'
-import { Dropdown, DropdownTrigger, DropdownMenu, DropdownItem, Button } from "@nextui-org/react";
+import React, { Suspense } from "react";
 import { Table, TableHeader, TableColumn, TableBody, TableRow, TableCell, Tooltip, Link } from "@nextui-org/react";
-import { useState, useEffect } from 'react';
+import classNames from "../../../../lib/tableClassNames";
 import axios from 'axios';
-import { Spacer } from '@nextui-org/react';
+import { useEffect } from 'react';
+import { useState } from 'react';
 import { DocumentIcon } from '@heroicons/react/24/solid'
 
-const ResAnaHome = () => {
-
-  const [semesters, setSemesters] = useState([])
-  const [selectedKeys, setSelectedKeys] = React.useState(new Set(["Select Academic Year"]));
-
-  const selectedValue = React.useMemo(
-    () => Array.from(selectedKeys).join(", ").replaceAll("_", " "),
-    [selectedKeys]
-  );
-
-  const getSemesters = async () => {
-    const response = await axios.get("/api/resana/getResAnaAcaYear?academic_year=" + selectedValue);
-    if (response)
-      setSemesters(response.data)
-  }
-
-  useEffect(() => {
-    getSemesters()
-  },[selectedKeys])
 
 
 
-  const columns = [
-    { name: "ACADEMIC YEAR", uid: "academic_year" },
-    { name: "SEMESTER", uid: "semester" },
-    { name: "VIEW", uid: "view" },
-  ];
+const ForeignStudentsList = () => {
 
-  const dropdownItems = [
-    {
-      key: "2022-23",
-      label: "2022-23",
-    },
-    {
-      key: "2021-22",
-      label: "2021-22",
-    },
-    {
-      key: "2020-21",
-      label: "2020-21",
-    },
-    {
-      key: "2019-20",
-      label: "2019-20",
+    const columns = [
+        { name: "ADMISSION YEAR", uid: "year" },
+        { name: "SEM 1", uid: "sem1" },
+        { name: "SEM 2", uid: "sem2" },
+        { name: "SEM 3", uid: "sem3" },
+        { name: "SEM 4", uid: "sem4" },
+        { name: "SEM 5", uid: "sem5" },
+        { name: "SEM 6", uid: "sem6" },
+        { name: "SEM 7", uid: "sem7" },
+        { name: "SEM 8", uid: "sem8" },
+    ];
+
+    const [list, setList] = useState([])
+
+    const getList = async () => {
+        const response = await axios.get("/api/resana/getResultAnalysis");
+        if (response)
+            setList(response.data)
     }
-  ];
 
-  const renderCell = React.useCallback((listItem, columnKey) => {
-    const cellValue = listItem[columnKey];
+    useEffect(() => {
+        getList()
+    }, [])
 
-    switch (columnKey) {
-      case "academic_year":
-        return (
-          <div className="flex flex-col">
-            <p className="font-bold	 text-left text-indigo-700">{cellValue}</p>
-          </div>
-        );
-    case "semester":
-            return (
-              <div className="flex flex-col">
-                <p className="font-bold	 text-left text-indigo-700">{cellValue}</p>
-              </div>
+    const renderCell = React.useCallback((listItem, columnKey) => {
+        const cellValue = listItem[columnKey];
+
+        switch (columnKey) {
+            case "name":
+                return (
+                    <div className="flex flex-col">
+                        <p className=" ">{cellValue}</p>
+                    </div>
+                );
+            case "year":
+                return (
+                    <div className="flex flex-col">
+                        <p className="text-bold  text-center ">{cellValue}</p>
+                    </div>
+                );
+            case "enrollment_no":
+                return (
+                    <div className="flex flex-col">
+                        <p className="text-bold text-center">{cellValue}</p>
+                    </div>
+                );
+            case "email":
+                return (
+                    <div className="flex flex-col">
+                        <p className="text-center  ">{cellValue}</p>
+                    </div>
+                );
+            default:
+              return (
+                <div className="flex flex-col">
+                    <p className="text-center  ">{cellValue}</p>
+                </div>
             );
-      case "view":
-        return (
-          <div className="flex flex-col ">
-            <Tooltip content="View Syllabus"  >
-              <Link href={listItem['file_url']}  color="primary"><DocumentIcon className="h-6 w-6 text-blue-500" /></Link>
-            </Tooltip>
-          </div>
-        );
-      default:
-        return cellValue;
-    }
-  }, []);
+        }
+    }, []);
 
-  return (
-    <div className="px-4 py-4 cardAboutDept">
-      <div className="flex flex-col items-center p-4  px-4 ">
-        <Dropdown>
-          <DropdownTrigger>
-            <Button
-              variant="shadow"
-              color='secondary'
-            >
-              {selectedValue}
-            </Button>
-          </DropdownTrigger>
-          <DropdownMenu aria-label="Dynamic Actions" items={dropdownItems} variant='solid' selectionMode="single"
-            selectedKeys={selectedKeys}
-            onSelectionChange={setSelectedKeys}>
-            {(item) => (
-              <DropdownItem
-                key={item.key}
-                color={item.key === "delete" ? "danger" : "default"}
-                className={item.key === "delete" ? "text-danger" : ""}
-              >
-                {item.label}
-              </DropdownItem>
-            )}
-          </DropdownMenu>
-        </Dropdown>
+    // const classNames = React.useMemo(
+    //   () => ({
+    //     th: ["bg-orange-100","font-sans","font-bold"],
+    //     td: [
+    //       // changing the rows border radius
+    //       // first
+    //       "group-data-[first=true]:first:before:rounded-none",
+    //       "group-data-[first=true]:last:before:rounded-none",
+    //       // middle
+    //       "group-data-[middle=true]:before:rounded-none",
+    //       // last
+    //       "group-data-[last=true]:first:before:rounded-none",
+    //       "group-data-[last=true]:last:before:rounded-none",
+    //       "bg-amber-50",
+    //       "text-zinc-700",
+    //       "font-sans",
+    //       "font-medium"
+    //     ],
+    //   }),
+    //   [],
+    // );
+
+    return (
+        <div className="px-4 cardAboutDept " >
+            <div className="box-border p-4 border-0 px-4">
+                <h1 className="font-sans text-4xl text-zinc-700 font-black uppercase text-center"> ----- Result Analysis -----</h1>
+            </div>
+            <div className="p-4  px-4 items-center  " >
+                <Table aria-label="Example table with custom cells" classNames={classNames} >
+                    <TableHeader columns={columns}>
+                        {(column) => (
+                            <TableColumn key={column.uid}>
+                                <p className="text-center">{column.name}</p>
+                            </TableColumn>
+                        )}
+                    </TableHeader>
+                    <TableBody items={list} >
+                        {(item) => (
+                            <TableRow key={item.id}>
+                                {(columnKey) => <TableCell>{renderCell(item, columnKey)}</TableCell>}
+                            </TableRow>
+                        )}
+                    </TableBody>
+                </Table>
+            </div>
         </div>
-        <div className="flex flex-col p-4  px-4 ">
-        <Spacer y={5} />
-
-        <Table aria-label="Example table with custom cells">
-          <TableHeader columns={columns}>
-            {(column) => (
-              <TableColumn key={column.uid}>
-                <p className="text-left text-default-700">{column.name}</p>
-              </TableColumn>
-            )}
-          </TableHeader>
-          <TableBody items={semesters}>
-            {(item) => (
-              <TableRow key={item.id}>
-                {(columnKey) => <TableCell>{renderCell(item, columnKey)}</TableCell>}
-              </TableRow>
-            )}
-          </TableBody>
-        </Table>
-      </div>
-    </div>
-  )
+    );
 }
 
-export default ResAnaHome
+
+
+export default ForeignStudentsList
