931fa41b250fa7c977db2dd22e1b42dc9c55c683
Tushar Gohil <tushar.gohil@scet.ac.in>
2024-01-11 11:41:21 +0530

    staff presently working

--- app/api/staff/getStaffList/route.js
+++ app/api/staff/getStaffList/route.js
@@ -4,6 +4,9 @@ import { NextResponse } from "next/server";
 export async function GET(request)
 {
   const staffs = await prisma.staff.findMany({
+    where: {
+      presently_working: 'yes'
+    },
     orderBy: {
               seqnum: 'asc',
             }
